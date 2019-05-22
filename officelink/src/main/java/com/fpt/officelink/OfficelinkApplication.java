573e5289df200e1cf18c05c84e35c7f23a5bf178
Quang <quangnguyenvietminh@gmail.com>
2019-05-22 11:48:25 +0700

    Init Spring Boot Project

---
+++ officelink/src/main/java/com/fpt/officelink/OfficelinkApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.officelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficelinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficelinkApplication.class, args);
+	}
+
+}
