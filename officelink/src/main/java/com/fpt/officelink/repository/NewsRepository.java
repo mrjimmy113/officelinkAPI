61f535b969dd1eb7b4bd7a85a9f78580088a8c80
CUONGTP\Thai Phu Cuong <icuong2302@gmail.com>
2019-07-02 14:54:38 +0700

    fix - CRUD news

--- officelink/src/main/java/com/fpt/officelink/repository/NewsRepository.java
+++ officelink/src/main/java/com/fpt/officelink/repository/NewsRepository.java
@@ -6,6 +6,8 @@
 package com.fpt.officelink.repository;
 
 import com.fpt.officelink.entity.News;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,9 +23,8 @@ import org.springframework.stereotype.Repository;
 public interface NewsRepository extends JpaRepository<News, Integer>{
     Page<News> findAllByTitleContainingAndIsDeleted(String title, Boolean isDeleted, Pageable page);
     
-    Optional<News> findByTitleAndIsDeleted(String title, Boolean isDeleted);
+    List<News> findByIsDeleted(boolean isDeleted);
     
-    Optional<News> findByShortDescriptionAndIsDeleted(String shortDescription, Boolean isDeleted);
-    
-    Optional<News> findByContentAndIsDeleted(String content, Boolean isDeleted);
+    @Query(value= "FROM News n WHERE n.dateCreated >= ?1 AND n.dateCreated <= ?2 ORDER BY n.dateCreated DESC")
+    List<News> findNewstByDateCreated(Date startDate, Date endDate) ;
 }
