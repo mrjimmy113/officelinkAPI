f60b0782f7163003eb50200c0495061767c27896
Quang <quangnguyenvietminh@gmail.com>
2019-07-08 13:01:05 +0700

    Load report

--- officelink/src/main/java/com/fpt/officelink/dto/SurveyReportDTO.java
+++ officelink/src/main/java/com/fpt/officelink/dto/SurveyReportDTO.java
@@ -1,9 +1,14 @@
 package com.fpt.officelink.dto;
 
+import java.sql.Date;
 import java.util.List;
 
 public class SurveyReportDTO {
 	private String name;
+	private Date startDate;
+	private Date endDate;
+	private int received;
+	private int sendOut;
 	private List<QuestionReportDTO> questions;
 
 	public String getName() {
@@ -21,8 +26,38 @@ public class SurveyReportDTO {
 	public void setQuestions(List<QuestionReportDTO> questions) {
 		this.questions = questions;
 	}
-	
-	
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public int getReceived() {
+		return received;
+	}
+
+	public void setReceived(int received) {
+		this.received = received;
+	}
+
+	public int getSendOut() {
+		return sendOut;
+	}
+
+	public void setSendOut(int sendOut) {
+		this.sendOut = sendOut;
+	}
 	
 	
 }
